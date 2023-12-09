7149990cb54ed3718650b7047320b0fd633716e6
PUPU <18795387502@163.com>
2023-12-09 18:44:38 +0800

    Delete data/ScaleQC directory

--- data/ScaleQC/data_download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 对于双端文件、采用cat file_1.fastq file_2.fastq > file.fastq 进行合并压缩
-for index in ERR174324; do
-  echo "${index}............................."
-  prefetch ${index}
-  fastq-dump --split-files ${index}
-  rm -rf ${index}
-done
