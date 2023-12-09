003d634dbc9a44fad6a3a5f0f3edcdd106e4c5ff
PUPU <18795387502@163.com>
2023-12-09 18:44:17 +0800

    Delete data/HARC directory

--- data/HARC/data_download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 对于双端文件、采用cat file_1.fastq file_2.fastq > file.fastq 进行合并压缩
-for index in SRR554369 SRR327342 SRR870667 ERR174310 ERR194146 ERP001775; do
-  echo "${index}............................."
-  prefetch ${index}
-  fastq-dump --split-files ${index}
-  rm -rf ${index}
-done
