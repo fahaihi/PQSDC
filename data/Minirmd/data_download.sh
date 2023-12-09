e9614b9f8e92ae1d708840d7d249cfa5eed0fa34
PUPU <18795387502@163.com>
2023-12-09 18:44:29 +0800

    Delete data/Minirmd directory

--- data/Minirmd/data_download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 对于双端文件、采用cat file_1.fastq file_2.fastq > file.fastq 进行合并压缩
-for index in SRR921889 SRR377645 SRR948355 SRR12175235; do
-  echo "${index}............................."
-  prefetch ${index}
-  fastq-dump --split-files ${index}
-  rm -rf ${index}
-done
